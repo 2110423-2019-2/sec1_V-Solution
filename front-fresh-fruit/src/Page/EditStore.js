0aba8287854e79a62fd21bb4b7ea848652c4c95c
dolphinep <ping.waragon@gmail.com>
2020-02-04 17:03:23 +0700

    mock fruits

--- front-fresh-fruit/src/Page/EditStore.js
+++ front-fresh-fruit/src/Page/EditStore.js
@@ -9,8 +9,8 @@ function Item(img, name) {
                     <h5 class="card-title">{name}</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <div class="edit-store-button">
-                        <a href="#" class="btn btn-primary edit-launch-botton">Launch</a>
-                        <a href="#" class="btn btn-primary">Reserve</a>
+                        <a href="#" class="btn btn-outline-success edit-launch-botton">Launch</a>
+                        <a href="#" class="btn btn-outline-warning">Reserve</a>
                     </div>
 
                 </div>
@@ -20,26 +20,43 @@ function Item(img, name) {
 }
 
 const EditStore = () => {
-    const mango = "https://f.btwcdn.com/store-34660/product/49e0623f-5e1d-9c4f-d254-5b7f69a49e9a.jpg"
-    const banana = "https://www.spcosmeticsplus.com/wp-content/uploads/2018/02/banana-extract.jpg"
-    const name = "Fruit"
+    const items = [
+        {
+            'img': 'https://f.btwcdn.com/store-34660/product/49e0623f-5e1d-9c4f-d254-5b7f69a49e9a.jpg',
+            'name': 'mango',
+            'isReservable': true,
+            'isLaunch': false
+        }, {
+            'img': 'https://www.spcosmeticsplus.com/wp-content/uploads/2018/02/banana-extract.jpg',
+            'name': 'banana',
+            'isReservable': false,
+            'isLaunch': false
+        }, {
+            'img': 'https://f.btwcdn.com/store-34660/product/466d4abc-625b-1010-6822-58a2ec549146.jpg',
+            'name': 'grape',
+            'isReservable': false,
+            'isLaunch': false
+        }, {
+            'img': 'https://cd.lnwfile.com/vkzco5.jpg',
+            'name': 'papaya',
+            'isReservable': false,
+            'isLaunch': false
+        }, {
+            'img': 'https://lh3.googleusercontent.com/proxy/r2ndY41plr6o69rdRxqGUNjdBcK3Z3q54Cz6XK-WLTe_VhpvK4UXlhl_mIF2JlsQHwAue_VtMaRJ2wJqnQXu6Hj9SkPo7KvEllfA4Q',
+            'name': 'orange',
+            'isReservable': false,
+            'isLaunch': false
+        }
+    ]
+    
     return (
         <div >
             <div class="edit-store-title">Your products</div>
             <div class="container">
                 <div class="row">
-                    {Item(mango, name)}
-                    {Item(banana, name)}
-                    {Item(mango, name)}
-                    {Item(banana, name)}
-                    {Item(mango, name)}
-                    {Item(banana, name)}
-                    {Item(banana, name)}
-                    {Item(banana, name)}
+                    {items.map((item) => Item(item.img,item.name))}
                 </div>
             </div>
-
-
         </div>
     );
 };
