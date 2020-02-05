c422c0d2f38ce5a99c10e7dcae3ff349326a4673
nutpattara <itsnutp@gmail.com>
2020-02-05 22:47:08 +0700

    add necessary model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,12 +32,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'users.apps.UsersConfig',
+	'product.apps.ProductConfig',
+	'frontend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
