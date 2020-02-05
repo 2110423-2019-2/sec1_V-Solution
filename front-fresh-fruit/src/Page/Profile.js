cf495824ff9472bcf404a7fa68a6a1fc4049d1ae
vachirachat <vachirachat@outlook.com>
2020-02-05 15:08:25 +0700

    add code profileCover and profilePic

--- front-fresh-fruit/src/Page/Profile.js
+++ front-fresh-fruit/src/Page/Profile.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import '../App.scss';
+import background from '../pictures/background.png';
+import ProfilePic from '../pictures/user.png';
 const Profile = () => {
     return (
-        <div class='row'>
-            <div class='col thaifont'>
-                
+        //style={{backgroundImage:`url(${background})`}}
+        <div>
+            <div>
+                <img class='profileCover' src={background} />
+                <img class='profilePic' src={ProfilePic} />
+
+                <h1 id='profilePic'>this is for cover page</h1>
+
             </div>
-            <div class='col'>
-                2
+            <div class='container'>
+                <div class='row'>
+                    <div class='col thaifont'>
+                        1
+                </div>
+                    <div class='col'>
+                        2
+                </div>
+                </div>
             </div>
         </div>
     );
