8e91a8c00f7e759a4e7f2d28620f0e607e9a28c2
vachirachat <vachirachat@outlook.com>
2020-02-06 09:14:49 +0700

    add provider folder

---
+++ front-fresh-fruit/src/Context/UserProvider.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserProvider = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UserProvider;
--- front-fresh-fruit/src/web-components/ProfileComponent.js
+++ front-fresh-fruit/src/web-components/ProfileComponent.js
@@ -14,28 +14,28 @@ const Profile = (props) => {
 
             </div>
             <div class='container thaifont'>
-                <div class='row'>    
-                    <h1 class='col-lg-10'>{props.username}</h1>
-                    <div class='logoAndEditInProfile'>
-                    <button type="button" class="btn btn-outline-info">logo</button>
-                    <button id='editInProfile' type="button" class="btn btn-outline-info">Edit</button>
+                <div class='row'>
+                    <h1 class='col-lg-10 col-6'>{props.username}</h1>
+                    <div class='logoAndEditInProfile col-lg-2 col-6'>
+                        <button type="button" class="btn btn-outline-info">logo</button>
+                        <button id='editInProfile' type="button" class="btn btn-outline-info">Edit</button>
                     </div>
                 </div>
                 <div class='row'>
                     <h2>{props.firstname}</h2>
-                    <h2>{props.lastname}</h2>
+                    <h2 style={{ marginLeft: '10px' }}>{props.lastname}</h2>
                 </div>
                 <div class='row'>
-                    <div class='col col-lg-2 thaifont'>
+                    <div class='col-lg-2 thaifont'>
                         <h3>Address :</h3>
                         <h3>Tel :</h3>
                         <h3>Birthdate :</h3>
-                </div>
-                    <div class='col'>
+                    </div>
+                    <div class='col-lg-10'>
                         <h3>{props.Address}</h3>
                         <h3>{props.Tel}</h3>
                         <h3>{props.Birthdate}</h3>
-                </div>
+                    </div>
                 </div>
             </div>
         </div>
