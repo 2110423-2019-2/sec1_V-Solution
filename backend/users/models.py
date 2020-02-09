1cc23926964accfd3c938ffe6079e61d8b5bc77b
ThreeTeaTree <teerawut0803@gmail.com>
2020-02-09 18:28:28 +0700

    update little model and serializers of user

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=30)
+
+    storeName = models.CharField(max_length=20)
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
