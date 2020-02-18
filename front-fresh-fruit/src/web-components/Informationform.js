cefe45275eee74bdc5ce97cbd71b11435e518075
vachirachat <vachirachat@outlook.com>
2020-02-18 10:59:26 +0700

    add hook in form but have bug in radio button

--- front-fresh-fruit/src/web-components/Informationform.js
+++ front-fresh-fruit/src/web-components/Informationform.js
@@ -2,9 +2,10 @@ import React,{useState} from 'react';
 import '../App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css'
 function Informationform() {
-    const [data,setData] = useState({});
+    const [data,setData] = useState({sex:''});
 
     const handleChange = (e) => {setData({...data,[e.target.name]:e.target.value})
+    console.log(data)
 }
     return (
 
@@ -21,7 +22,7 @@ function Informationform() {
                 <div class="form-group row">
                     <div class='col-form-label col-sm-2' style={{ position: 'static', left: '0px' }}><label style={{ color: "red" }}>*</label><label>Username:</label></div>
                     <div class="col-sm-6">
-                        <input class="form-control" type="text" name="name" style={{ marginLeft: '10px' }} placeholder="" onChange={handleChange} />
+                        <input class="form-control" type="text" name="username" style={{ marginLeft: '10px' }} placeholder="" onChange={handleChange} />
                     </div>
                 </div>
 
@@ -36,48 +37,48 @@ function Informationform() {
                 <div class="form-group row">
                     <div class='col-form-label col-sm-2' style={{ position: 'static', left: '0px' }}><label style={{ color: "red" }}>*</label><label>Surname:</label></div>
                     <div class='col-sm-6'>
-                        <input class="form-control" type="text" name="name" style={{ marginLeft: '10px' }} placeholder="" />
+                        <input class="form-control" type="text" name="surname" style={{ marginLeft: '10px' }} placeholder="" />
                     </div></div>
 
                 <div class="form-group row">
                     <div class='col-form-label col-sm-2' style={{ position: 'static', left: '0px' }}><label style={{ color: "red" }}>*</label><label>Email:</label></div>
                     <div class='col-sm-6'>
-                        <input class="form-control" type="email" name="name" style={{ marginLeft: '10px' }} placeholder="someone@outlook.com" />
+                        <input class="form-control" type="email" name="email" style={{ marginLeft: '10px' }} placeholder="someone@outlook.com" onChange={handleChange}/>
                     </div></div>
 
                 <div class="form-group row">
                     <div class='col-form-label col-sm-2' style={{ position: 'static', left: '0px' }}><label style={{ color: "red" }}>*</label><label>Password:</label></div>
                     <div class='col-sm-6'>
-                        <input class="form-control" type="password" name="name" style={{ marginLeft: '10px' }} placeholder="" aria-describedby="passwordHelp" onChange={handleChange}/>
+                        <input class="form-control" type="password" name="password" style={{ marginLeft: '10px' }} placeholder="" aria-describedby="passwordHelp" onChange={handleChange}/>
                         <small id='passwordHelp' class="form-text text-muted">between 6 - 30 characters including alphabet and number</small>
                     </div></div>
 
                 <div class="form-group row">
                     <div class='col-form-label col-sm-2' style={{ position: 'static', left: '0px' }}><label style={{ color: "red" }}>*</label><label>Address:</label></div>
                     <div class='col-sm-6'>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" style={{ marginLeft: '10px' }}></textarea>
+                        <textarea class="form-control" name='address' rows="3" style={{ marginLeft: '10px' }} onChange={handleChange}></textarea>
                     </div></div>
 
                 <div class="form-group row">
                     <div class='col-form-label col-sm-2' style={{ position: 'static', left: '0px' }}><label style={{ color: "red" }}>*</label><label>Tel:</label></div>
                     <div class='col-sm-6'>
-                        <input class="form-control" type="Tel" name="name" style={{ marginLeft: '10px' }} placeholder="" />
+                        <input class="form-control" type="number" name="tel" style={{ marginLeft: '10px' }} placeholder="" onChange={handleChange}/>
                     </div></div>
 
                 <div class="form-group row">
                     <div class='col-form-label col-sm-2' style={{ position: 'static', left: '0px' }}><label style={{ color: "red" }}>*</label><label>Birthdate:</label></div>
                     <div class='col-sm-6'>
-                        <input class="form-control" type="date" name="name" style={{ marginLeft: '10px' }} placeholder="" />
+                        <input class="form-control" type="date" name="Birthdate" style={{ marginLeft: '10px' }} placeholder="" onChange={handleChange}/>
                     </div></div>
                 <div class='form-group row'>
                 <legend class="col-form-label col-sm-2">Sex</legend>
                 <div class="col-sm-3">
                 <div class="form-check form-check-inline">
-  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
+  <input class="form-check-input" type="radio" name="sex" id="inlineRadio1" value="Male" onChange={handleChange}/>
   <label class="form-check-label" for="inlineRadio1">Male</label>
 </div>
 <div class="form-check form-check-inline">
-  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+  <input class="form-check-input" type="radio" name="sex" id="inlineRadio2" value="Female" onChange={handleChange}/>
   <label class="form-check-label" for="inlineRadio2">Female</label>
 </div>
       </div>
@@ -86,7 +87,7 @@ function Informationform() {
                             <div class="form-group row">
                                 <div class='col-form-label col-sm-2' style={{ position: 'static', left: '0px' }}><label style={{ color: "red" }}>*</label><label>NationalID:</label></div>
                                 <div class='col-sm-6'>
-                                    <input class="form-control" type="number" name="name" style={{ marginLeft: '10px' }} placeholder="" />
+                                    <input class="form-control" type="number" name="nationalID" onChange={handleChange} style={{ marginLeft: '10px' }} placeholder="x-xxxx-xxxxx-xx-x" />
                                 </div></div>
 
             <div class='col-sm-8'>
