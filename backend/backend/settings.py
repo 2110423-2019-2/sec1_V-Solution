44d5d3e338b3f31142fadec4faf807011f8f1b5f
nutpattara <itsnutp@gmail.com>
2020-02-19 16:43:56 +0700

    added cors header

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     "sslserver",
+    'corsheaders',
     'profile',
     'product',
     'django.contrib.admin',
@@ -53,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
