7b72bc9882933c4f28d513be3566ed83b1273a22
dolphinep <ping.waragon@gmail.com>
2020-03-10 23:30:17 +0700

    remove footer

--- front-fresh-fruit/src/Page/HomePage.js
+++ front-fresh-fruit/src/Page/HomePage.js
@@ -40,52 +40,7 @@ const HomePage = () => {
 
       {/* --------------- footer part ----------------------- */}
 
-      <div class="container-fluid" style={{backgroundColor: "black"}}>
-
-        <div class="row" style={{height: "10%"}}>
-          <div class="col">
-            <div class="row">
-              <h2 style={{color:"white", fontWeight: "bold", marginLeft: "20px", marginTop: "10px"}}>Customer Support</h2>
-            </div>
-            <div class="row" style={{ marginTop: "10px"}}>
-              <div class="col" style={{marginLeft: "20px"}}>
-                  <div class="row">
-                    <h6 style={{color: "white"}}>Email: support@hotmail.com</h6>
-                  </div>
-                  <div class="row">
-                    <h6 style={{color: "white"}}>Tel: 02-XXXXXXXX</h6>
-                  </div>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="row">
-              <h2 style={{color:"white", fontWeight: "bold", marginTop: "10px"}}>Customer Address</h2>
-            </div>
-            <div class="row" style={{marginTop: "10px"}}>
-              <div class="col">
-                <div class="row">
-                  <h6 style={{color: "white"}}>Chulalongkorn University</h6>
-                </div>
-                <div class="row">
-                  <h6 style={{color: "white"}}>254 Phayathai Road, Pathumwan, Bangkok 10330 Thailand</h6>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="row" style={{height: "50px"}}>
-          <div class="col">
-            <h4 style={{fontsize: "2px", color: "white", marginTop: "12px"}}>Fresh Fruit @ 2020 All Right Reserved</h4>
-          </div>
-          <div class="col">
-            <h4 style={{fontsize: "2px", color: "white", textAlign:"right", marginTop: "12px"}}>Privacy Term</h4>
-          </div>
-        </div>
-
-
-      </div>
+      
 
       
     </div>
