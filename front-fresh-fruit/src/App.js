4f0043d55c631ea11dde49eb4bb0229a737bca0e
Mawin <monde_sm@hotmail.com>
2020-03-11 14:25:40 +0700

    Edit profile page and change name

--- front-fresh-fruit/src/App.js
+++ front-fresh-fruit/src/App.js
@@ -4,12 +4,12 @@ import Footer from './web-components/Footer';
 import Home from './Page/HomePage';
 import Register from './Page/Register';
 import RegisterSeller from './Page/RegisterSeller';
-import Profile from './Page/Profile';
+import ProfileCus from './Page/ProfileCus';
 import Seller from './Page/Seller';
 import Signin from './Page/Signin';
 import Store from './Page/Store';
 import AddItem from './Page/AddItem';
-import EditProfile from './Page/EditProfile';
+import ProfileSeller from './Page/ProfileSeller';
 import EditStore from './Page/EditStore';
 import UserContext from './Context/UserContext';
 
@@ -54,15 +54,15 @@ function App() {
         {/* body part */}
         <Switch>
           <UserContext.Provider value={{isloggedin:`${isloggedin}`,setLogin:handleIsloggedin,setToken:handleSetToken,clearToken:clearToken,getToken:getToken}}>
-          <Route exact path='/' component={Home} />
-          {localStorage.getItem('Token')!=null ? (<Route path='/profile' component={Profile} />):(<Route path='/profile' component={Seller} />)}
+          <Route exact path='/' component={ProfileCus} />
+          {localStorage.getItem('Token')!=null ? (<Route path='/profile' component={ProfileSeller} />):(<Route path='/profile' component={Seller} />)}
           
           <Route path='/seller' component={Seller} />
           <Route path='/register' component={Register} />
           <Route path='/registerSeller' component={RegisterSeller} />
           <Route path='/Signin' component={Signin} />
           <Route path='/store' component={Store} />
-          <Route path='/EditProfile' component={EditProfile} />
+          <Route path='/ProfileSeller' component={ProfileSeller} />
           <Route path='/EditStore' component={EditStore} />
           <Route path='/addItem' component={AddItemform} />
           </UserContext.Provider>
--- front-fresh-fruit/src/web-components/ProfileComponent.js
+++ front-fresh-fruit/src/web-components/ProfileComponent.js
@@ -1,9 +1,9 @@
 import React,{useState,useEffect} from 'react';
 import '../App.scss';
 import axios from 'axios';
-import background from '../pictures/background.png';
-import ProfilePic from '../pictures/user.png';
-import UserContext from '../Context/UserContext';
+import Background from '../pictures/seller.jpg'
+import Mond from '../pictures/profile pic.png'
+import EditPic from '../pictures/edit.png'
 import { useHistory } from 'react-router-dom';
 const Profile = (props) => {
     const history = useHistory();
@@ -11,54 +11,76 @@ const Profile = (props) => {
     
     return (
         //style={{backgroundImage:`url(${background})`}}
-        <div>
-            <div>
-                <img class='profileCover' src={background} />
-                <img class='profilePic' src={ProfilePic} />
-            </div>
-            <div class='container thaifont'>
-                <div class='row'>
-                    <h1 class='col-lg-10 col-6'>{props.username}</h1>
-                    <div class='logoAndEditInProfile col-lg-2 col-6'>
-                        <button type="button" class="btn btn-outline-info">Logo</button>
-                        <button id='editInProfile' type="button" class="btn btn-outline-info">Edit</button>
-                    </div>
-                </div>
-                <div class='row'>
-                    <h2>{props.firstname}</h2>
-                    <h2 style={{ marginLeft: '10px' }}>{props.lastname}</h2>
-                </div>
-                <div class='row'>
-                    <div class='col-lg-2 thaifont'>
-                        <h3>Address :</h3>
-                        <h3>Tel :</h3>
-                        <h3>Birthdate :</h3>
-                    </div>
-                    <div>{props.id}</div>
-                    <div class='col-lg-10'>
-                        <h3>{props.Address}</h3>
-                        <h3>{props.Tel}</h3>
-                        <h3>{props.Birthdate}</h3>
-                    </div>
-                </div>
+        <div style={{backgroundColor:"#6AC17D", width: "1520px", height: "995px"}}>
+            
+            
+            <div style={{width: "1200px", marginLeft: "160px"}}>
                 
-            <UserContext.Consumer>
-                            {({isloggedin,setLogin,clearToken,usertoken,getToken}) => (
-                                <div>
-                                <h1>{String(isloggedin)}</h1>
-                                <h1>{getToken()}</h1>
-                                
-                                <button class='btn btn-primary' onClick={(e)=>{
-                                    e.preventDefault();
-                                    clearToken()
-                                    history.push('/')
-                                    
-                                }}>Log out</button>
+                {/* head */}
+
+                <div style={{backgroundColor:"#6AC17D", height:"100px"}}>
+
+                </div>
+
+                
+                {/* body */}
+
+                <div style={{height: "995px", backgroundColor: "#E6FFEC"}}>
+                    
+                    <div class="row">
+
+                        <div class="col-3">
+                            <img src={Mond} style={{width: "200px", marginTop: "-70px", marginLeft: "50px"}} />
+                        </div>
+                        <div class="col">
+                            <h1 style={{fontFamily: "Marker Felt", color:"#36368D", fontSize:"50px", marginTop:"50px", marginLeft:"30px"}}>
+                                Mondlnwza007</h1> 
+                        </div>
+
+                        <div class="col-3">
+                            <div class="row" style={{marginTop:"50px"}}>
+                                <div class="col">
+                                    <h1 style={{marginLeft: "80px"}}><img src={EditPic}/></h1>
                                 </div>
-                            )}
-                        </UserContext.Consumer>
+                                <div class="col">
+                                    <h1 style={{fontFamily: "Marker Felt", color:"#36368D"}}>Edit</h1>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div style={{marginTop:"35px", marginLeft: "350px"}}>
+                        <h2 style={{fontFamily: "Marker Felt", color:"#36368D"}}>Mawin Siangyai</h2>
+                    </div>
+
+                    <div style={{ height: "350px"}}>
+                        
+                        <div style={{marginTop: "60px"}}>
+                            <div style={{marginLeft: "180px"}}>
+                                <h1 style={{fontFamily: "Marker Felt", color:"#36368D", fontSize:"40px"}}>Address : Ihouse</h1>
+                            </div>
+                            <br/><br/>
+                            <div style={{marginLeft: "260px"}}>
+                                <h1 style={{fontFamily: "Marker Felt", color:"#36368D", fontSize:"40px"}}> Tel : 084-xxxxxxx</h1>
+                            </div>
+                            <br/><br/>
+                            <div style={{marginLeft: "155px"}}>
+                                <h1 style={{fontFamily: "Marker Felt", color:"#36368D", fontSize:"40px"}}> Birthdate : 31 February 1998</h1>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    
+
+                </div>
+
             </div>
-        </div>
+            
+
+
+        </div>  
 
     );
 };
