cdedcd6ecafdf4b894790ac2072f70a05dce26fa
Mawin <monde_sm@hotmail.com>
2020-03-11 14:55:28 +0700

    ProfileCustomer

--- front-fresh-fruit/src/Page/ProfileCus.js
+++ front-fresh-fruit/src/Page/ProfileCus.js
@@ -1,23 +1,88 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import '../App.scss';
-import ProfileComponent from '../web-components/ProfileComponent';
-import UserContext from '../Context/UserContext';
-
-const ProfileCus = (props) => {
-    //for setup fetch data
+import axios from 'axios';
+import Background from '../pictures/seller.jpg'
+import Mond from '../pictures/profile pic.png'
+import EditPic from '../pictures/edit.png'
+import { useHistory } from 'react-router-dom';
+const Profile = (props) => {
+    const history = useHistory();
+    console.log(props.id);
+    
     return (
         //style={{backgroundImage:`url(${background})`}}
-    
-            <div>
-                <ProfileComponent 
-                username='vachirachat' firstname='vachirachat' lastname='saw' Address='this is my address' Tel='this is my tel' Birthdate='this is my birthdate'
-                number = {props.ID} />
+        <div style={{backgroundColor:"#6AC17D", width: "1520px", height: "995px"}}>
+            
+            
+            <div style={{width: "1200px", marginLeft: "160px"}}>
                 
-            </div>
-        
+                {/* head */}
 
+                <div style={{backgroundColor:"#6AC17D", height:"100px"}}>
+
+                </div>
+
+                
+                {/* body */}
+
+                <div style={{height: "995px", backgroundColor: "#E6FFEC"}}>
+                    
+                    <div class="row">
+
+                        <div class="col-3">
+                            <img src={Mond} style={{width: "200px", marginTop: "-70px", marginLeft: "50px"}} />
+                        </div>
+                        <div class="col">
+                            <h1 style={{fontFamily: "Marker Felt", color:"#36368D", fontSize:"50px", marginTop:"50px", marginLeft:"30px"}}>
+                                Mondlnmza007</h1> 
+                        </div>
+
+                        <div class="col-3">
+                            <div class="row" style={{marginTop:"50px"}}>
+                                <div class="col">
+                                    <h1 style={{marginLeft: "80px"}}><img src={EditPic}/></h1>
+                                </div>
+                                <div class="col">
+                                    <h1 style={{fontFamily: "Marker Felt", color:"#36368D"}}>Edit</h1>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div style={{marginTop:"35px", marginLeft: "350px"}}>
+                        <h2 style={{fontFamily: "Marker Felt", color:"#36368D"}}>Mawin Siangyai</h2>
+                    </div>
+
+                    <div style={{ height: "350px"}}>
+                        
+                        <div style={{marginTop: "60px"}}>
+                            <div style={{marginLeft: "180px"}}>
+                                <h1 style={{fontFamily: "Marker Felt", color:"#36368D", fontSize:"40px"}}>Address : Ihouse</h1>
+                            </div>
+                            <br/><br/>
+                            <div style={{marginLeft: "260px"}}>
+                                <h1 style={{fontFamily: "Marker Felt", color:"#36368D", fontSize:"40px"}}> Tel : 084-xxxxxxx</h1>
+                            </div>
+                            <br/><br/>
+                            <div style={{marginLeft: "155px"}}>
+                                <h1 style={{fontFamily: "Marker Felt", color:"#36368D", fontSize:"40px"}}> Birthdate : 31 February 1998</h1>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    
+
+                </div>
+
+            </div>
+            
+
+
+        </div>  
 
     );
 };
 
-export default ProfileCus;
+export default Profile;
