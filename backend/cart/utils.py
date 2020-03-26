c8ceea8c8a032de7c8af2422c7bd2a05d0af630e
nutpattara <itsnutp@gmail.com>
2020-03-26 22:57:50 +0700

    cart bug fix

--- backend/cart/utils.py
+++ backend/cart/utils.py
@@ -42,14 +42,12 @@ def decrease_entry(cart, product, amount):
         return cart
     
     new_amount = max(old_entry.quantity - amount, 0)
-    if new_amount == 0:
-        old_entry.delete()
-        return cart
-
-    ## FIX HERE
     amount_de = old_entry.quantity - new_amount
     old_entry.quantity = new_amount
-    old_entry.save()
+    if old_entry.quantity == 0:
+        old_entry.delete()
+    else:
+        old_entry.save()
 
     cart.count -= amount_de
     cart.total_price -= calculate_product_price(product) * amount_de
