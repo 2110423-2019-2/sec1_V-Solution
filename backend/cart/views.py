2ef0522a3b2927d4520cd01e8ca50420c6dcc9d5
nutpattara <itsnutp@gmail.com>
2020-03-26 22:49:33 +0700

    Improve cart api

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -17,65 +17,13 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.http import request
 from django.http import HttpResponse , JsonResponse
 
-from .models import Entry
+from .models import *
+from .utils import * 
+from .serializers import *
 from profile.models import Profile
 from product.models import Product
-from cart.models import Cart
-from cart.serializers import cart_to_list
 from product.utility import calculate_product_price
 import purchase
-
-##
-def add_entry(cart, product, amount):
-    try:
-        old_entry = Entry.objects.get(cart=cart, product=product)
-    except Entry.DoesNotExist:
-        old_entry = None
-
-    if old_entry:
-        old_entry.quantity += amount
-        old_entry.save()
-    else:
-        entry = Entry.objects.create(
-            product = product,
-            cart = cart,
-            quantity = amount
-        )
-    
-    cart.count += amount
-    cart.total_price += (calculate_product_price(product) * amount)
-    cart.total_deliver_price += (product.deliverPrice * amount)
-    cart.save()
-    return cart
-
-def decrease_entry(cart, product, amount):
-    try:
-        old_entry = Entry.objects.get(cart=cart, product=product)
-    except Entry.DoesNotExist:
-        return cart
-    
-    new_amount = old_entry.quantity - amount
-    if new_amount <= 0: 
-        new_amount = 0
-        old_entry.delete()
-        return cart
-
-    ## FIX HERE
-    amount_de = old_entry.quantity - new_amount
-    old_entry.quantity = new_amount
-    old_entry.save()
-
-    cart.count -= amount_de
-    cart.total_price -= calculate_product_price(product) * amount_de
-    cart.total_deliver_price -= product.deliverPrice * amount_de
-    cart.save()
-    return cart
-
-def clear_cart_entry(cart):
-    entries = Entry.objects.filter(cart=cart)
-    for entry in entries:
-        entry.delete()
-        return cart
 
 ## add entry
 @api_view(["POST"])
