8ea4ef151c375e2c3f7429a03a280e298aa4ce59
vachirachat <vachirachat@outlook.com>
2020-04-02 01:51:41 +0700

    fix css in my purchase

--- front-fresh-fruit/src/web-components/PurchaseComponent.js
+++ front-fresh-fruit/src/web-components/PurchaseComponent.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import _purchaseComponent from '../styles/_purchaseComponent.scss';
 const PurchaseComponent = (props) => {
+
+    const cardBody = (name,price,deliveryDate,amount,status) => {
+        return (
+            <div class="card-body">
+                <h5 class="card-title">Product : {name}</h5>
+                <div style={{ display: 'flex', }}>
+                    <p class="card-text">Price : {price} Bath</p>
+                    <p class="card-text">Date : {deliveryDate}</p>
+
+                    <p class="card-text">Amount : {amount} </p>
+                    
+                </div>
+            </div>
+        )
+    }
     return (
         <div>
             <div class="card">
                 <div class="card-header">
                     <h4>Transation number 1</h4>
-                    <div class='status'>
-                    {props.status=='padding' ? <h4><p class='badge badge-danger'>Padding...</p></h4> : <h4><p class='badge badge-success'>Already delivered</p></h4>}
+                    <div class='status' style={{display:'flex'}}>
+                        {props.status == 'padding' ? <h4><p class='badge badge-danger'>Padding...</p></h4> : <h4><p class='badge badge-success'>Already delivered</p></h4>}
+                        {props.status == 'padding' ? <button type="button" class="btn btn-success btn-sm" style={{height:'30px'}}>Pay now</button> : <p></p>}
                     </div>
+                    
                 </div>
                 <div class="card-body">
-                    <div class="card-body">
-                        <h5 class="card-title">Product : {props.name}</h5>
-                        <div style={{display:'flex',}}>
-                        <p class="card-text">Price : {props.price} Bath</p>
-                        <p class="card-text">Date : {props.deliveryDate}</p>
-                        
-                        <p class="card-text">Amount : {props.amount} </p>
-                        {props.status=='padding' ? <button type="button" class="btn btn-success" style={{marginLeft:'50%'}}>Pay now</button> : <p></p>}
-                        </div>
-                    </div>
-                </div>
+                {cardBody('mango',100,'10-mar-2562',50,'padding')}
                 </div>
             </div>
+        </div>
     );
 };
 
