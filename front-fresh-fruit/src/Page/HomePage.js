c7ae8d6b8fbbbe2997011cbe0ea4de32d33184af
dolphinep <ping.waragon@gmail.com>
2020-04-20 21:55:45 +0700

    fix search missing in mobile

--- front-fresh-fruit/src/Page/HomePage.js
+++ front-fresh-fruit/src/Page/HomePage.js
@@ -57,16 +57,20 @@ useEffect(() => {
 
       <div class="container-fluid" style={{ backgroundColor: "#6AC17D" }}>
 
-        <div class="row" style={{ height: "700px" }}>
+        <div class="row" style={{ paddingTop:'5%', paddingBottom:'10%' }}>
 
           <div class="col">
 
             <div class="row" style={{ marginTop: "120px", marginLeft: "50px" }}>
               <div class="col" style={{ textAlign: "center" }}><img src={homefruit} style={{ height: '400px', width: '600px' }} /></div>
-              <div class="col" style={{ textAlign: "center", color: "white" }}><h1>Find Daily & Organic fruit</h1>
+              <div class="col" style={{ textAlign: "center", color: "white" }}>
+                <h1>Find Daily & Organic fruit</h1>
                 <h2>with</h2><h1>FRESHFRUIT</h1>
-                <input type="text" name="search" style={{ marginTop: "40px", width: "500px", height: "45px", borderRadius: "20px"}}  onChange={handleChange} />
+                <div>
+                  <input type="text" name="search" style={{ marginTop: "40px", width: "500px", lineHeight: "45px", borderRadius: "20px"}}  onChange={handleChange} />
                 <h1>{searchQuery['product_name']}</h1>
+                </div>
+                
                 {/*<div style={{ marginTop: "40px" }}><button style={{ width: '120px', height: '40px', borderRadius: "20px" }} onClick={getProduct}>Search</button></div>*/}
               </div>
             </div>
